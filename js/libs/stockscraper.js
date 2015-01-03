066974955f321d6e6f1d95617110086d3416f522
adeng <adeng@media.ucla.edu>
2015-01-02 21:34:16 -0800

    Preliminary Balance Sheet Scraper

---
+++ js/libs/stockscraper.js
@@ -0,0 +1,22 @@
+function getBalanceSheet( quote )
+{
+	var requestURL = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.balancesheet%20WHERE%20symbol=%27' + quote + '%27%20AND%20timeframe=%27annual%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+
+	console.log( requestURL );
+
+	$.ajax(
+    {
+    	url: requestURL,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        success: function ( json ) {
+        	var sArray = json['query']['results']['balancesheet']['statement'];
+
+        	if( typeof sArray != "undefined" )
+       			genBalanceSheet( sArray );
+       		else
+       			$("#bsSection").append("<p id='balanceSheetTable'>We're sorry, this quote does not appear to have any data.</p>");
+        }
+    });
+}
---
+++ js/section-scripts/balanceSheet.js
@@ -0,0 +1,55 @@
+var data;
+
+function genBalanceSheet( statementArray ) {
+	$("#balanceSheetTable").remove();
+	data = statementArray;
+	var tableString = "";
+	// Generate header
+	tableString += '<table id="balanceSheetTable" class="table table-hover">' +
+    					'<thead><tr>' +
+    						'<th class="text-center">Asset</th>' +
+    						'<th></th>' +
+    						'<th></th>' +
+    						'<th class="text-center">' + statementArray[0]['period'] + '</th>' +
+    						'<th class="text-center">' + statementArray[1]['period'] + '</th>' +
+    						'<th class="text-center">' + statementArray[2]['period'] + '</th>' +
+    					'</tr></thead><tbody>';
+
+    tableString += '<tr><td><b>Assets</b></td><td></td><td></td><td></td><td></td><td></td>';
+
+   	for( var i = 0; i < Object.keys( statementArray[0] ).length; i++ )
+	{
+		var asset = Object.keys( statementArray[0] )[i];
+		var display = asset;
+		if( asset == "period" )
+			continue;
+
+		tableString += '<tr>';
+
+		if( asset.indexOf( "Total" ) != -1 )
+			tableString += '<b>';
+
+		tableString += '<td>&nbsp;&nbsp;&nbsp;' + display + '</td>';
+		tableString += '<td></td>';
+		tableString += '<td></td>';
+		tableString += '<td class="text-right">' + ( statementArray[0][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[0][asset]['content'] ), "" ) ) + '</td>';
+		tableString += '<td class="text-right">' + ( statementArray[1][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[1][asset]['content'] ), "" ) ) + '</td>';
+		tableString += '<td class="text-right">' + ( statementArray[2][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[2][asset]['content'] ), "" ) ) + '</td>';
+
+		if( asset.indexOf( "Total" ) != -1 )
+			tableString += '</b>';
+
+		tableString += '</tr>';
+
+		if( asset == "TotalAssets" )
+			tableString += '<tr><td><b>Liabilities</b></td><td></td><td></td><td></td><td></td><td></td>';
+
+		if( asset == "TotalLiabilities" )
+			tableString += '<tr><td><b>Equity</b></td><td></td><td></td><td></td><td></td><td></td>';
+	}
+
+    // ....annnnnd Fin :D
+    tableString += '</tbody></table>';
+
+    $("#bsSection").append( tableString );
+}
