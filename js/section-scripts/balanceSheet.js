ddb8c8d405ff8e0da07aad7d3ded4b5236702321
adeng <adeng@media.ucla.edu>
2015-01-02 23:12:50 -0800

    Commit some other ratios

--- js/section-scripts/balanceSheet.js
+++ js/section-scripts/balanceSheet.js
@@ -92,14 +92,40 @@ function genRatios( statementArray ) {
     						'<th class="text-right">' + statementArray[2]['period'] + '</th>' +
     					'</tr></thead><tbody>';
 
-    // Generate rest
+    // Generate header
+    tableString += '<tr><td><b>Liquidity Ratios</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+
+    // Generate current ratio
     tableString += '<tr>';
-    tableString += '<td>Current Ratio</td>';
+    tableString += '<td>&nbsp;&nbsp;&nbsp;Current Ratio</td>';
     tableString += '<td></td>';
     tableString += '<td></td>';
     tableString += '<td class="text-right">' + currentRatio( statementArray[0]['TotalCurrentAssets']['content'], statementArray[0]['TotalCurrentLiabilities']['content'] ) + '</td>';
     tableString += '<td class="text-right">' + currentRatio( statementArray[1]['TotalCurrentAssets']['content'], statementArray[1]['TotalCurrentLiabilities']['content'] ) + '</td>';
     tableString += '<td class="text-right">' + currentRatio( statementArray[2]['TotalCurrentAssets']['content'], statementArray[2]['TotalCurrentLiabilities']['content'] ) + '</td>';
+    tableString += '</tr>';
+
+    // Generate quick ratio
+    tableString += '<tr>';
+    tableString += '<td>&nbsp;&nbsp;&nbsp;Quick Ratio</td>';
+    tableString += '<td></td>';
+    tableString += '<td></td>';
+    tableString += '<td class="text-right">' + quickRatio( statementArray[0]['CashAndCashEquivalents']['content'], statementArray[0]['ShortTermInvestments']['content'], statementArray[0]['NetReceivables']['content'], statementArray[0]['TotalCurrentLiabilities']['content'] ) + '</td>';
+    tableString += '<td class="text-right">' + quickRatio( statementArray[1]['CashAndCashEquivalents']['content'], statementArray[1]['ShortTermInvestments']['content'], statementArray[1]['NetReceivables']['content'], statementArray[1]['TotalCurrentLiabilities']['content'] ) + '</td>';
+    tableString += '<td class="text-right">' + quickRatio( statementArray[2]['CashAndCashEquivalents']['content'], statementArray[2]['ShortTermInvestments']['content'], statementArray[2]['NetReceivables']['content'], statementArray[2]['TotalCurrentLiabilities']['content'] ) + '</td>';
+    tableString += '</tr>';
+
+    // Another header
+    tableString += '<tr><td><b>Leverage Ratios</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+
+    // Generate debt-equity ratio
+    tableString += '<tr>';
+    tableString += '<td>&nbsp;&nbsp;&nbsp;Debt-Equity Ratio</td>';
+    tableString += '<td></td>';
+    tableString += '<td></td>';
+    tableString += '<td class="text-right">' + debtEquityRatio( statementArray[0]['LongTermDebt']['content'], statementArray[0]['Short_CurrentLongTermDebt']['content'], statementArray[0]['TotalStockholderEquity']['content'] ) + '</td>';
+    tableString += '<td class="text-right">' + debtEquityRatio( statementArray[1]['LongTermDebt']['content'], statementArray[1]['Short_CurrentLongTermDebt']['content'], statementArray[1]['TotalStockholderEquity']['content'] ) + '</td>';
+    tableString += '<td class="text-right">' + debtEquityRatio( statementArray[2]['LongTermDebt']['content'], statementArray[2]['Short_CurrentLongTermDebt']['content'], statementArray[2]['TotalStockholderEquity']['content'] ) + '</td>';
     tableString += '</tr>';
 
     // ....annnnnd Fin :D
@@ -112,12 +138,48 @@ function genRatios( statementArray ) {
 // Calculator functions
 function currentRatio( currAssets, currLiabilities )
 {
-	console.log( currAssets );
-	console.log( currLiabilities );
 	var ca = parseInt( currAssets );
 	var cl = parseInt( currLiabilities );
-	if( ca == 0 || cl == 0 || ca * 1 != ca || cl * 1 != cl )
+	if( cl == 0 || ca * 1 != ca || cl * 1 != cl )
 		return "-";
 	else
 		return ( ca / cl ).toFixed( 2 );
+}
+
+function quickRatio( cash, sti, receive, currLiabilities )
+{
+	var ca = 0;
+	if( parseInt( cash ) * 1 == parseInt( cash ) )
+		ca += parseInt( cash );
+	if( parseInt( sti ) * 1 == parseInt( sti ) )
+		ca += parseInt( sti );
+	if( parseInt( receive ) * 1 == parseInt( receive ) )
+		ca += parseInt( receive );
+	
+	var cl = parseInt( currLiabilities );
+	if( cl == 0 || ca * 1 != ca || cl * 1 != cl )
+		return "-";
+	else
+		return ( ca / cl ).toFixed( 2 );
+}
+
+function debtEquityRatio( ltdebt, stdebt, tsEquity )
+{
+
+	console.log( ltdebt );
+	console.log( stdebt );
+	console.log( tsEquity );
+	console.log( "-" );
+	var de = 0;
+	if( parseInt( ltdebt ) * 1 == parseInt( ltdebt ) )
+		de += parseInt( ltdebt );
+	if( parseInt( stdebt ) * 1 == parseInt( stdebt ) )
+		de += parseInt( stdebt );
+
+	var te = parseInt( tsEquity );
+
+	if( te == 0 || de * 1 != de || te * 1 != te )
+		return "-";
+	else
+		return ( de / te ).toFixed( 2 );
 }
