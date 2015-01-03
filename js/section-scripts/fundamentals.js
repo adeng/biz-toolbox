7f809989c40992d75f1e8fea496817894898a7f1
adeng <adeng@media.ucla.edu>
2015-01-03 10:49:27 -0800

    Separated fundamentals and financials

--- js/section-scripts/fundamentals.js
+++ js/section-scripts/fundamentals.js
@@ -1,68 +1,9 @@
-function genIncomeStatement( statementArray ) {
-	$("#loading").remove();
-	var tableString = "";
-	var current = false;
-	// Generate header
-	tableString += '<h4 id="incomeHeader">Income Statement for ' + quote.value + '</h4>';
-
-	tableString += '<table id="incomeStatementTable" class="table table-hover">' +
-    					'<thead><tr>' +
-    						'<th class="text-center"></th>' +
-    						'<th></th>' +
-    						'<th></th>' +
-    						'<th class="text-right">' + statementArray[0]['period'] + '</th>' +
-    						'<th class="text-right">' + statementArray[1]['period'] + '</th>' +
-    						'<th class="text-right">' + statementArray[2]['period'] + '</th>' +
-    					'</tr></thead><tbody>';
-
-    // Generate rest
-
-   	for( var i = 0; i < Object.keys( statementArray[0] ).length; i++ )
-	{
-		var asset = Object.keys( statementArray[0] )[i];
-		var display = asset;
-		if( asset == "period" )
-			continue;
-
-		tableString += '<tr><td>';
-
-		if( asset == "CostofRevenue" );
-			tableString += "Cost of Revenue";
-		else if( asset == "ResearchDevelopment" )
-			tableString += "Research and Development";
-		else if( asset == "SellingGeneralandAdministrative" )
-			tableString += "Selling, General, and Admin Expenses";
-		else if( asset == "NonRecurring" )
-			tableString += "Non-Recurring Expenses";
-		else if( asset == "OperatingIncomeorLoss" )
-			tableString += "Net Operating Income";
-		else if( asset == "TotalOtherIncome_ExpensesNet" )
-			tableString += "Net Other Income/Expenses";
-		else
-			tableString += unCamel( display );
-
-		tableString += '</td>';
-		tableString += '<td></td>';
-		tableString += '<td></td>';
-		tableString += '<td class="text-right">' + ( statementArray[0][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[0][asset]['content'] ), "" ) ) + '</td>';
-		tableString += '<td class="text-right">' + ( statementArray[1][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[1][asset]['content'] ), "" ) ) + '</td>';
-		tableString += '<td class="text-right">' + ( statementArray[2][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[2][asset]['content'] ), "" ) ) + '</td>';
-
-		tableString += '</tr>';
-	}
-
-    // ....annnnnd Fin :D
-    tableString += '</tbody></table>';
-
-    $("#icSection").append( tableString );
-}
-
 function genRatios( statementArray ) {
 	$("#loading").remove();
 	var tableString = "";
 
 	// Generate header
-	tableString += '<h4 id="balanceHeader">Balance Sheet Ratios for ' + quoteR.value + '</h4>';
+	tableString += '<h4 id="balanceHeader">Ratios for ' + quoteBS.value + '</h4>';
 
 	tableString += '<table id="balanceSheetTable" class="table table-hover">' +
     					'<thead><tr>' +
@@ -159,7 +100,7 @@ function genRatios( statementArray ) {
     // ....annnnnd Fin :D
     tableString += '</tbody></table>';
 
-    $("#bsrSection").append( tableString );
+    $("#raSection").append( tableString );
 
 }
 
