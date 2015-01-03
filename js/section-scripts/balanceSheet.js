f73aa3cfb79b085cf0c1c99bab31b94d6f708bfc
adeng <adeng@media.ucla.edu>
2015-01-02 22:09:11 -0800

    Some bolding and headers

--- js/section-scripts/balanceSheet.js
+++ js/section-scripts/balanceSheet.js
@@ -1,6 +1,7 @@
 function genBalanceSheet( statementArray ) {
 	$("#loading").remove();
 	var tableString = "";
+	var current = false;
 	// Generate header
 	tableString += '<h4 id="balanceHeader">Balance Sheet for ' + quote.value + '</h4>';
 
@@ -14,7 +15,9 @@ function genBalanceSheet( statementArray ) {
     						'<th class="text-center">' + statementArray[2]['period'] + '</th>' +
     					'</tr></thead><tbody>';
 
-    tableString += '<tr><td><b>Assets</b></td><td></td><td></td><td></td><td></td><td></td>';
+    tableString += '<tr><td><b>Assets</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+    tableString += '<tr><td><b>&nbsp;&nbsp;&nbsp;Current Assets</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+    current = true;
 
    	for( var i = 0; i < Object.keys( statementArray[0] ).length; i++ )
 	{
@@ -24,27 +27,39 @@ function genBalanceSheet( statementArray ) {
 			continue;
 
 		tableString += '<tr>';
+		tableString += '<td>&nbsp;&nbsp;&nbsp;';
+
+		if( asset.indexOf( "TotalCurrent" ) != -1 ) 
+			current = false;
+
+		if( current )
+			tableString += '&nbsp;&nbsp;&nbsp;';
 
 		if( asset.indexOf( "Total" ) != -1 )
-			tableString += '<b>';
+			tableString += "<b>";
 
-		tableString += '<td>&nbsp;&nbsp;&nbsp;' + display + '</td>';
+		tableString += display;
+
+		if( asset.indexOf( "Total" ) != -1 )
+			tableString += "</b>";
+
+		tableString += '</td>';
 		tableString += '<td></td>';
 		tableString += '<td></td>';
 		tableString += '<td class="text-right">' + ( statementArray[0][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[0][asset]['content'] ), "" ) ) + '</td>';
 		tableString += '<td class="text-right">' + ( statementArray[1][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[1][asset]['content'] ), "" ) ) + '</td>';
 		tableString += '<td class="text-right">' + ( statementArray[2][asset]['content'] == "-" ? "-" : accountify( parseInt( statementArray[2][asset]['content'] ), "" ) ) + '</td>';
 
-		if( asset.indexOf( "Total" ) != -1 )
-			tableString += '</b>';
-
 		tableString += '</tr>';
 
-		if( asset == "TotalAssets" )
-			tableString += '<tr><td><b>Liabilities</b></td><td></td><td></td><td></td><td></td><td></td>';
+		if( asset == "TotalAssets" ) {
+			tableString += '<tr><td><b>Liabilities</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+			current = true;
+			tableString += '<tr><td><b>&nbsp;&nbsp;&nbsp;Current Liabilities</b></td><td></td><td></td><td></td><td></td><td></td></tr>';
+		}
 
 		if( asset == "TotalLiabilities" )
-			tableString += '<tr><td><b>Equity</b></td><td></td><td></td><td></td><td></td><td></td>';
+			tableString += '<tr><td><b>Equity</b></td><td></td><td></td><td></td><td></td><td></td></tr><tr></tr>';
 	}
 
     // ....annnnnd Fin :D
